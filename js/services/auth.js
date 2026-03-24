70b96001e416db6da24b7eec0072d727ae40655d
Albany072001 <albataro2001@gmail.com>
2026-03-23 22:37:20 -0400

    File provide the necessary authenthication credentials for API access

---
+++ js/services/auth.js
@@ -0,0 +1,5 @@
+export function getAuthHeader() { // Return the authentication header for API requests
+  return {
+    Authorization: `Basic Y29hbGl0aW9uOnNraWxscy10ZXN0` // Basic auth token
+  };
+}
