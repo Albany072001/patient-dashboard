3df01cf84044bb3ea716c29fb5b340feb2451177
Albany072001 <albataro2001@gmail.com>
2026-04-15 18:55:12 -0400

    Changed the datastatedisplay from block to flex

--- js/ui/renderPatientProfile.js
+++ js/ui/renderPatientProfile.js
@@ -72,5 +72,5 @@ export function renderPatientProfile(patient) { // Render the patient profile in
   container.querySelector(".error-state").style.display = "none";
 
   dataState.innerHTML = html;
-  dataState.style.display = "block"; // Show the data state with the rendered patient profile information.  
+  dataState.style.display = "flex"; // Show the data state with the rendered patient profile information.  
 }
